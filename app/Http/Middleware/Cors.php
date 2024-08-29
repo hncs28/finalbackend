a98bfb90573a81b2dd4eb3d2c972c7b69525cf9f
namchaser <namblue28092002@gmail.com>
2024-08-29 08:25:55 +0700

    cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    
+    public function handle($request, Closure $next)
+    {
+        //Intercepts OPTIONS requests
+        if ($request->isMethod('OPTIONS')) {
+            $response = response('', 200);
+        } else {
+            // Pass the request to the next middleware
+            $response = $next($request);
+        }
+
+        $IlluminateResponse = 'Illuminate\Http\Response';
+        $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
+
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
+            'Access-Control-Allow-Headers' => 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Authorization , Access-Control-Request-Headers',
+        ];
+        // Adds headers to the response
+        # Laravel
+        if ($response instanceof $IlluminateResponse) {
+            $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+            $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+            $response->header('Access-Control-Allow-Origin', '*');
+
+            return $response;
+        }
+
+        # Symfony
+        if ($response instanceof $SymfonyResopnse) {
+            foreach ($headers as $key => $value) {
+                $response->headers->set($key, $value);
+            }
+            return $response;
+        }
+        // Sends it
+        return $response;
+    }
+}
