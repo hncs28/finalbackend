71cf4be073871b8a29467f943d5ae5690e462d8a
Dao Duy Thong <duythong.ptit@gmail.com>
2024-09-05 16:43:57 +0700

    Update Cors.php : test using 1 domain in allow origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -21,7 +21,7 @@ class Cors
         $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
 
         $headers = [
-            'Access-Control-Allow-Origin' => 'https://somedia-five.vercel.app https://so-media-git-duythong-thongisme0207gmailcoms-projects.vercel.app',
+            'Access-Control-Allow-Origin' => 'https://so-media-git-duythong-thongisme0207gmailcoms-projects.vercel.app',
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
             'Access-Control-Allow-Headers' => 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Authorization , Access-Control-Request-Headers',
             'Access-Control-Allow-Credentials' => 'true',
