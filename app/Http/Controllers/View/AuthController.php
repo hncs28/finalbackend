a50fbfa8570efdc65e9b202711c110636317a316
Duy Thong <duythong.ptit@gmail.com>
2024-09-10 00:30:39 +0700

    Refactor form validation and authentication in AuthController.php

--- app/Http/Controllers/View/AuthController.php
+++ app/Http/Controllers/View/AuthController.php
@@ -14,36 +14,43 @@ class AuthController extends Controller
     {
         return view('CMS/login');
     }
+
     public function login(Request $request)
     {
         $credentials = $request->validate([
             'email' => ['required'],
             'password' => ['required'],
         ]);
+
         \Log::info('Auth params', ['params' => $credentials]);
+
         $check = Auth::attempt($credentials);
-        $project = DB::table('projects')->select('*');
-        $project = $project->get();
+
+        \Log::info('Auth attempt result', ['result' => $check]);
+
         if ($check) {
-            return view('CMS/projects/projects',compact('project'));
+            \Log::info('Login successful');
+            return redirect()->route('projects.index'); // Chuyển hướng đến route 'projects.index'
         } else {
+            \Log::error('Login failed');
             return back()->withErrors([
                 'username' => 'The provided credentials do not match our records.',
             ]);
         }
-    
     }
 
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $user = new User;
         $user->name = $request->input('name');
-        $user->email = $request->input('email');    
+        $user->email = $request->input('email');
         $user->password = Hash::make($request->input('password'));
         $user->save();
         \Log::info('store success', ['data' => $user]);
     }
-    
-    public function logout(Request $request) {
+
+    public function logout(Request $request)
+    {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
